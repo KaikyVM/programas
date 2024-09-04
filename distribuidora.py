38f9c5373f12decae39d301f5ed2052638383378
Kaiky <156738016+KaikyVM@users.noreply.github.com>
2024-09-04 10:56:49 -0300

    distribuidora
    4) Dado o valor de faturamento mensal de uma distribuidora, detalhado por estado:
    • SP – R$67.836,43
    • RJ – R$36.678,66
    • MG – R$29.229,88
    • ES – R$27.165,48
    • Outros – R$19.849,53
    
    Escreva um programa na linguagem que desejar onde calcule o percentual de representação que cada estado teve dentro do valor total mensal da distribuidora.  

---
+++ distribuidora.py
@@ -0,0 +1,18 @@
+# Faturamento mensal por estado (em R$)
+faturamento_estados = {
+    "SP": 67836.43,
+    "RJ": 36678.66,
+    "MG": 29229.88,
+    "ES": 27165.48,
+    "Outros": 19849.53
+}
+
+# Calculando o faturamento total
+faturamento_total = sum(faturamento_estados.values())
+
+# Calculando o percentual de representação de cada estado
+percentuais = {estado: (valor / faturamento_total) * 100 for estado, valor in faturamento_estados.items()}
+
+
+for estado, percentual in percentuais.items():
+    print(f"{estado}: {percentual:.2f}%")
